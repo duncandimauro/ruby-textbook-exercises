bfa9d4e960b0cde57b0ee625ca7d56eb79e839c2
Duncan Di Mauro <102009384+duncandimauro@users.noreply.github.com>
2023-05-02 21:45:04 -0700

    Added files for the "Variables" chapter

---
+++ variables/name.rb
@@ -0,0 +1,11 @@
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+name = first_name + " " + last_name
+
+puts "Hello " + name
+
+10.times {puts name}
